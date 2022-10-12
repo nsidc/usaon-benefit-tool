3d4fca3e70d6e79898265a8f474cd5a340ec3f91
Matt Fisher <matt.fisher@nsidc.org>
2022-10-12 17:35:37 -0600

    Show list of surveys page

--- usaon_vta_survey/routes/surveys.py
+++ usaon_vta_survey/routes/surveys.py
@@ -1,11 +1,12 @@
 from flask import render_template
 
 from usaon_vta_survey import app
+from usaon_vta_survey.models.tables import Survey
 
 
 @app.route('/surveys')
 def surveys():
-    surveys = [1, 2]
+    surveys = Survey.query.order_by(Survey.created_timestamp).all()
     return render_template(
         'surveys.html',
         surveys=surveys,
