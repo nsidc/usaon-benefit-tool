05e76cd697423c82d4add321489aa63e4a43b765
Matt Fisher <matt.fisher@nsidc.org>
2022-10-20 17:42:37 -0600

    Implement diagram from real data

---
+++ usaon_vta_survey/util/sankey.py
@@ -0,0 +1,31 @@
+from itertools import chain
+
+from usaon_vta_survey.models.tables import Response, ResponseDataProduct
+
+
+def applications_sankey(response: Response) -> list[list[str | int]]:
+    """Provide Sankey data structure of applications, formatted for Highcharts."""
+    # Convert tuples to lists for passing to Javascript-land:
+    data = list(list(e) for e in _applications_sankey(response))
+    return data
+
+
+def _applications_sankey(response: Response) -> list[tuple[str, str, int]]:
+    """Provide a sankey data structure of applications, formatted for type checker."""
+    data = list(chain(
+        *[
+            _data_product_application_sankey_links(data_product)
+            for data_product in response.data_products
+        ]
+    ))
+    return data
+
+
+def _data_product_application_sankey_links(
+    data_product: ResponseDataProduct,
+) -> list[tuple[str, str, int]]:
+    data = [
+        (data_product.name, r.application.name, r.performance_rating)
+        for r in data_product.output_relationships
+    ]
+    return data
