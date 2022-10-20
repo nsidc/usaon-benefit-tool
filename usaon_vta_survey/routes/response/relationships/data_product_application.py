4728927e3e8f25fa8324333e6799d8843ecb417a
Matt Fisher <matt.fisher@nsidc.org>
2022-10-20 15:12:29 -0600

    Hacky implementation of dataproduct-application relationship form

--- usaon_vta_survey/routes/response/relationships/data_product_application.py
+++ usaon_vta_survey/routes/response/relationships/data_product_application.py
@@ -1,4 +1,6 @@
 from flask import redirect, render_template, request, url_for
+from flask_wtf import FlaskForm
+from wtforms import FormField
 
 from usaon_vta_survey import app, db
 from usaon_vta_survey.forms import FORMS_BY_MODEL
@@ -15,32 +17,27 @@ from usaon_vta_survey.models.tables import (
     methods=['GET', 'POST'],
 )
 def view_response_data_product_application_relationships(survey_id: str):
-    """View and add application/dataproduct relationships to a response."""
+    """View and add application/dataproduct relationships to a response.
+
+    TODO: Refactor this whole pile of stuff.
+    """
     application_id = request.args.get('application_id')
     data_product_id = request.args.get('data_product_id')
 
-    ApplicationForm = FORMS_BY_MODEL[ResponseApplication]
-    DataProductForm = FORMS_BY_MODEL[ResponseDataProduct]
-    DataProductApplicationForm = FORMS_BY_MODEL[ResponseDataProductApplication]
-
     survey = db.get_or_404(Survey, survey_id)
 
-    if data_product_id:
-        response_data_product = db.get_or_404(ResponseDataProduct, data_product_id)
-    else:
-        response_data_product = ResponseDataProduct(response_id=survey.response_id)
+    class SuperForm(FlaskForm):
+        """Combine all necessary forms into one super-form.
 
-    if application_id:
-        response_application = db.get_or_404(ResponseApplication, application_id)
-    else:
-        response_application = ResponseApplication(response_id=survey.response_id)
+        NOTE: Additional class attributes are added dynamically below.
+        """
+        relationship = FormField(FORMS_BY_MODEL[ResponseDataProductApplication])
 
     if data_product_id and application_id:
         # If not found, will be `None`
-        response_data_product_application = db.get(
+        response_data_product_application = db.session.get(
             ResponseDataProductApplication,
-            response_data_product_id,
-            response_application_id,
+            (data_product_id, application_id),
         )
     else:
         response_data_product_application = None
@@ -48,31 +45,59 @@ def view_response_data_product_application_relationships(survey_id: str):
     if response_data_product_application is None:
         response_data_product_application = ResponseDataProductApplication()
 
+    if data_product_id:
+        response_data_product = db.get_or_404(ResponseDataProduct, data_product_id)
+        response_data_product_application.response_data_product_id = data_product_id
+    else:
+        response_data_product = ResponseDataProduct(response_id=survey.response_id)
+        SuperForm.data_product = FormField(FORMS_BY_MODEL[ResponseDataProduct])
+
+    if application_id:
+        response_application = db.get_or_404(ResponseApplication, application_id)
+        response_data_product_application.response_application_id = application_id
+    else:
+        response_application = ResponseApplication(response_id=survey.response_id)
+        SuperForm.application = FormField(FORMS_BY_MODEL[ResponseApplication])
+
+    form_obj = {
+        'data_product': response_data_product,
+        'application': response_application,
+        # NOTE: Logic below depends on relationship being last in this dict
+        'relationship': response_data_product_application,
+    }
+
     if request.method == 'POST':
-        # form = Form(request.form, obj=response_application)
-        ...
+        form = SuperForm(request.form, obj=form_obj)
 
         if form.validate():
-            ...
-            # form.populate_obj(response_application)
-            # db.session.add(response_application)
-            # db.session.commit()
+            # Add only submitted sub-forms into the db session
+            for key, obj in form_obj.items():
+                if hasattr(form, key):
+                    form[key].form.populate_obj(obj)
+                    db.session.add(obj)
 
-        # return redirect(url_for('view_response_applications', survey_id=survey.id))
-        return ...
+                    # Update the relationship object with the ids of any new entities 
+                    if key != 'relationship':
+                        db.session.flush()
+                        db.session.refresh(obj)
+                        setattr(
+                            response_data_product_application,
+                            f'response_{key}_id',
+                            obj.id,
+                        )
 
-    application_form = ApplicationForm(obj=response_application)
-    data_product_form = DataProductForm(obj=response_data_product)
-    relationship_form = DataProductApplicationForm(obj=response_data_product_application)
+            db.session.commit()
+
+        return redirect(url_for('view_response_applications', survey_id=survey.id))
+
+    form = SuperForm(obj=form_obj)
     return render_template(
         'response/relationships/data_product_application.html',
+        form=form,
         survey=survey,
         data_product=response_data_product,
         data_products=survey.response.data_products,
-        data_product_form=data_product_form,
         application=response_application,
         applications=survey.response.applications,
-        application_form=application_form,
         relationship=response_data_product_application,
-        relationship_form=relationship_form,
     )
