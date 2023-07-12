d9deb46303b4785029a1dc269a3c386d0222fa9d
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-07-12 14:00:43 -0600

    remove format from __init__

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,10 +1,8 @@
 from invoke import Collection
 
 from . import db, env, test
-from . import format as format_
 
 ns = Collection()
 ns.add_collection(db)
 ns.add_collection(env)
-ns.add_collection(format_)
 ns.add_collection(test)
