842663acfe7534ca7aeeb1ae9808ac7503f644ed
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-07-13 16:19:50 -0600

    re-add routes/__init__.py to get app working
    adjust so auto formtter does not remove the contents again

---
+++ usaon_vta_survey/routes/__init__.py
@@ -0,0 +1,7 @@
+import usaon_vta_survey.routes.response  # ruff: noqa
+import usaon_vta_survey.routes.response.applications  # ruff: noqa
+import usaon_vta_survey.routes.response.data_products  # ruff: noqa
+import usaon_vta_survey.routes.response.observing_systems  # ruff: noqa
+import usaon_vta_survey.routes.response.relationships.data_product_application  # ruff: noqa
+import usaon_vta_survey.routes.survey  # ruff: noqa
+import usaon_vta_survey.routes.surveys  # root route
