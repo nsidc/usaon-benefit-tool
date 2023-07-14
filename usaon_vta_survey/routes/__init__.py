dbf1d14c2fceba45463c8f82d6a8140fd848d6d1
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-07-14 09:57:13 -0600

    Add google sso login to routes

--- usaon_vta_survey/routes/__init__.py
+++ usaon_vta_survey/routes/__init__.py
@@ -3,5 +3,6 @@ import usaon_vta_survey.routes.response.applications  # ruff: noqa
 import usaon_vta_survey.routes.response.data_products  # ruff: noqa
 import usaon_vta_survey.routes.response.observing_systems  # ruff: noqa
 import usaon_vta_survey.routes.response.relationships.data_product_application  # ruff: noqa
+import usaon_vta_survey.routes.google  # ruff: noqa
 import usaon_vta_survey.routes.survey  # ruff: noqa
 import usaon_vta_survey.routes.surveys  # root route
