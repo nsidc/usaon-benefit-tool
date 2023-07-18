b7ae399fd76502e95255112a1112c503ac651bbf
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-07-18 16:03:35 -0600

    Add inputs on profile page and update DB user record

--- usaon_vta_survey/routes/profile.py
+++ usaon_vta_survey/routes/profile.py
@@ -1,10 +1,20 @@
-from flask import redirect, session, url_for
+from flask import render_template, request
 
 from usaon_vta_survey import app, db
+from usaon_vta_survey.models.tables import User
 
-@app.route('/profile/<id>', methods=['POST','GET'])
+
+@app.route('/profile/<id>', methods=['POST', 'GET'])
 def profile(id):
-    id = db.session.get(User, user.id)
-    
+    user = db.session.get(User, id)
+    # breakpoint()
+    if request.method == 'POST':
+        user.biography = request.form['biography']
+        user.orcid = request.form['orcid']
+        user.affiliation = request.form['affiliation']
+
+        db.session.add(user)
+        db.session.commit()
+        # return redirect(url_for('h'))
 
     return render_template('profile.html', user=user)
