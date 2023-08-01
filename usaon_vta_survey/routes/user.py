386ae26e25d378da69f378135a23b815e36b918c
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-08-01 17:01:34 -0600

    WIP block role change from non-admins

--- usaon_vta_survey/routes/user.py
+++ usaon_vta_survey/routes/user.py
@@ -1,5 +1,5 @@
 from flask import render_template, request
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user
 
 from usaon_vta_survey import app, db
 from usaon_vta_survey.forms import FORMS_BY_MODEL
@@ -13,6 +13,14 @@ def load_user(user_id: str) -> User:
     return User.query.get(user_id)
 
 
+def _validate_role_change(user: User, form) -> None:
+    # TODO: if role is changed, check if current user is admin
+    # if not an admin do not allow role change, display error
+    # if form.data.is_changed():
+    if not current_user.role_id == 'admin':
+        raise RuntimeError("Only admins can edit this field")
+
+
 @app.route('/user/<user_id>', methods=['POST', 'GET'])
 def user(user_id: str):
     Form = FORMS_BY_MODEL[User]
@@ -21,6 +29,7 @@ def user(user_id: str):
     if request.method == 'POST':
         form = Form(request.form, obj=user)
         if form.validate():
+            _validate_role_change(user, form)
             form.populate_obj(user)
             db.session.add(user)
             db.session.commit()
