3bcbadc9b0b5eabad7053bb850cf3a0fe7ad034a
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-08-02 16:10:38 -0600

    Change elif to if

--- usaon_vta_survey/util/authorization.py
+++ usaon_vta_survey/util/authorization.py
@@ -4,7 +4,7 @@ from flask_login import current_user
 def limit_response_editors() -> None:
     if not current_user.is_authenticated:
         raise RuntimeError("Please login")
-    elif current_user.role_id not in ['admin', 'respondent']:
+    if current_user.role_id not in ['admin', 'respondent']:
         raise RuntimeError(
             "You must be a respondent or admin to respond to this survey."
         )
