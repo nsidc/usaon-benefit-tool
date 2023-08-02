b2a05a03015d6df12c680f4fdf9f018e78ac20b5
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-08-02 13:53:51 -0600

    Add error for when a user is not logged in.

--- usaon_vta_survey/util/authorization.py
+++ usaon_vta_survey/util/authorization.py
@@ -2,7 +2,13 @@ from flask_login import current_user
 
 
 def limit_response_editors() -> None:
-    if not (current_user.role_id == 'admin' or current_user.role_id == 'respondent'):
-        raise RuntimeError(
-            "You must be a respondent or admin to respond to this survey."
-        )
+    try:
+        if not (
+            current_user.role_id == 'admin' or current_user.role_id == 'respondent'
+        ):
+            raise RuntimeError(
+                "You must be a respondent or admin to respond to this survey."
+            )
+    except AttributeError as AE:
+        # TODO: Add link to login here?
+        raise RuntimeError("Please login to use this application.") from AE
