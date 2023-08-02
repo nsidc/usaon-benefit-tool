b35e5e07043b559a321229dd8e7d2ee3131b62ad
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-08-02 11:33:16 -0600

    Add analyst role and only restrict responses to new survey.

--- usaon_vta_survey/routes/response/__init__.py
+++ usaon_vta_survey/routes/response/__init__.py
@@ -1,14 +1,25 @@
 from flask import render_template
+from flask_login import current_user
 
 from usaon_vta_survey import app, db
 from usaon_vta_survey.models.tables import Response, Survey
 
 
+def _limit_response_editors() -> None:
+    if not (current_user.role_id == 'admin' or current_user.role_id == 'respondent'):
+        raise RuntimeError(
+            "You must be a respondent or admin to respond to this survey."
+        )
+
+
 @app.route('/response/<string:survey_id>', methods=['GET'])
 def view_response(survey_id: str):
     """View or create response to a survey."""
+    # Anyone should be able to view a survey
+    # Only admins or respondents should be able to create a response.
     survey = db.get_or_404(Survey, survey_id)
     if not survey.response:
+        _limit_response_editors()
         response = Response()
         survey.response = response
 
