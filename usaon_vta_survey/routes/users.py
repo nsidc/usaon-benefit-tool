0712967fb4ac199971021745f470bd6f65d2b246
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-08-02 09:08:30 -0600

    Revert unused changes.

--- usaon_vta_survey/routes/users.py
+++ usaon_vta_survey/routes/users.py
@@ -1,16 +1,13 @@
 from flask import render_template
 
 from usaon_vta_survey import app
-from usaon_vta_survey.forms import RoleEdit
 from usaon_vta_survey.models.tables import User
 
 
 @app.route('/users')
 def view_users():
     users = User.query.order_by(User.name).all()
-    form = RoleEdit()
     return render_template(
         'users.html',
         users=users,
-        form=form,
     )
