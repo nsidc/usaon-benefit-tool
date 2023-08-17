00918f983ecb1e7f2bc663de408539eeb030cc02
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-08-17 12:36:07 -0600

    WIP almost there making SBA relationship page act like others

--- usaon_vta_survey/routes/__init__.py
+++ usaon_vta_survey/routes/__init__.py
@@ -3,6 +3,7 @@ import usaon_vta_survey.routes.response.applications
 import usaon_vta_survey.routes.response.data_products
 import usaon_vta_survey.routes.response.observing_systems
 import usaon_vta_survey.routes.response.sbas
+import usaon_vta_survey.routes.response.relationships.application_societal_benefit_area
 import usaon_vta_survey.routes.response.relationships.data_product_application
 import usaon_vta_survey.routes.response.relationships.observing_system_data_product
 import usaon_vta_survey.routes.login
