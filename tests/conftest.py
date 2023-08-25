f05e88287564c3d0526b7a8e314e1cfb9401a86a
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-08-25 16:34:04 -0600

    WIP magicmock

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
+from unittest import mock
+
 import pytest
+from flask_login import FlaskLoginClient
+from mock_alchemy.mocking import UnifiedAlchemyMagicMock
 
 from usaon_vta_survey import app as my_app
+from usaon_vta_survey.models.tables import User
 
 
 @pytest.fixture()
@@ -11,3 +16,23 @@ def app():
 @pytest.fixture()
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture()
+def session_with_user():
+    session = UnifiedAlchemyMagicMock(
+        data=[
+            (
+                [mock.call.query(User), mock.call.get(1)],
+                [User(id=1, email='foo@example.com', name='foo bar', role_id='admin')],
+            )
+        ]
+    )
+    return session
+
+
+@pytest.fixture()
+def client_logged_in(app, session_with_user):
+    app.test_client_class = FlaskLoginClient
+    user = session_with_user.query(User).get(1)
+    return app.test_client(user=user)
