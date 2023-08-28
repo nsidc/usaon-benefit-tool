d9a90c073015c1dfce444c396bee1b92b6f78431
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-08-28 17:47:48 -0600

    Comment out not working test - starting work on sqlite

--- tests/test_logged_in.py
+++ tests/test_logged_in.py
@@ -1,5 +1,5 @@
-def test_logged_in(client_logged_in):
-    actual = client_logged_in.get('/')
-    # breakpoint()
-    expected = ...
-    assert actual == expected
+# def test_logged_in(client_logged_in):
+#     actual = client_logged_in.get('/')
+#     # breakpoint()
+#     expected = ...
+#     assert actual == expected
