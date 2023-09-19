724c533869e7abb2c630a6f2e57411033de7f908
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-09-19 13:49:44 -0600

    LoginManager

--- usaon_vta_survey/routes/user.py
+++ usaon_vta_survey/routes/user.py
@@ -1,5 +1,5 @@
 from flask import flash, render_template, request
-from flask_login import current_user
+from flask_login import LoginManager, current_user
 
 from usaon_vta_survey import db
 from usaon_vta_survey.forms import FORMS_BY_MODEL
@@ -7,9 +7,11 @@ from usaon_vta_survey.models.tables import User
 from usaon_vta_survey.routes.root import root_blueprint
 from usaon_vta_survey.util.db.setup import app
 
+login_manager = LoginManager(app)
 
-# TODO: discover if this is porrible with app factory
-# @login_manager.user_loader
+
+# TODO: discover if this is possible with app factory
+@login_manager.user_loader
 def load_user(user_id: str) -> User:
     return User.query.get(user_id)
 
