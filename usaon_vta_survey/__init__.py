af2192a7e677b0be9681cada35e038a65b5c1c66
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-09-19 10:42:02 -0600

    Fix import

--- usaon_vta_survey/__init__.py
+++ usaon_vta_survey/__init__.py
@@ -45,9 +45,9 @@ def create_app():
     db.init_app(app)
     Bootstrap5(app)
 
-    from usaon_vta_survey.routes import root
+    from usaon_vta_survey.routes.root import root_blueprint
 
-    app.register_blueprint(root.root_blueprint)
+    app.register_blueprint(root_blueprint)
 
     from usaon_vta_survey.routes import response
 
