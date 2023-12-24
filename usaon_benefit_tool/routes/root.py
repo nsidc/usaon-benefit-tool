5f75170ebfa9239a4d48f4c05684def52d780829
Sherwin <varghesesheriwn@gmail.com>
2023-12-25 00:01:57 +0530

    added fixes

--- usaon_benefit_tool/routes/root.py
+++ usaon_benefit_tool/routes/root.py
@@ -1,10 +1,8 @@
 from flask import Blueprint, render_template
 
-from usaon_benefit_tool.constants import repo
-
 root_bp = Blueprint('root', __name__, url_prefix='/')
 
 
 @root_bp.route('')
 def root():
-    return render_template('home.html', url=repo.REPO_URL)
+    return render_template('home.html')
