f4663da4e9ed80d4404f8eefd5578a5880253b56
Matt Fisher <mfisher87@gmail.com>
2024-03-01 15:08:19 -0700

    Add an invoke task to load test data

--- tasks/db.py
+++ tasks/db.py
@@ -4,7 +4,7 @@ from .util import in_container
 
 
 @task(aliases=('recreate',))
-def init(ctx, *, load=True):
+def init(ctx, *, reference_data=True, test_data=False):
     """Drop and recreate all database tables, loading them by default."""
     if not in_container():
         print(
@@ -23,9 +23,11 @@ def init(ctx, *, load=True):
         print('Recreating tables...')
         recreate_tables_()
 
-        if load:
-            print('Loading reference tables...')
+        if reference_data:
             load_reference_data(ctx)
+
+        if test_data:
+            load_test_data(ctx)
 
 
 @task()
@@ -44,4 +46,25 @@ def load_reference_data(ctx):
     app = create_app()
 
     with app.app_context():
+        print('Loading reference tables...')
         populate_reference_data()
+
+
+@task()
+def load_test_data(ctx):
+    """Populate operational tables with test data."""
+    if not in_container():
+        print(
+            'Please run from the container context using'
+            ' `scripts/invoke_in_container.sh`',
+        )
+        return
+
+    from usaon_benefit_tool import create_app
+    from usaon_benefit_tool.util.db.setup import populate_test_data
+
+    app = create_app()
+
+    with app.app_context():
+        print('Loading test data...')
+        populate_test_data()
