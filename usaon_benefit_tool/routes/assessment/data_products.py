f4b49a8ee91a09ee083834c9f0cace4e7797b979
Robyn Marowitz <robyn.marowitz@colorado.edu>
2024-03-07 16:51:32 -0700

    project -> assessment final route location

--- usaon_benefit_tool/routes/assessment/data_products.py
+++ usaon_benefit_tool/routes/assessment/data_products.py
@@ -7,7 +7,7 @@ from usaon_benefit_tool.models.tables import Survey, SurveyDataProduct
 from usaon_benefit_tool.util.authorization import limit_response_editors
 from usaon_benefit_tool.util.sankey import sankey_subset
 
-project_data_products_bp = Blueprint(
+assesment_data_products_bp = Blueprint(
     'data_products',
     __name__,
     url_prefix='/data_products',
@@ -15,56 +15,56 @@ project_data_products_bp = Blueprint(
 Form = FORMS_BY_MODEL[SurveyDataProduct]
 
 
-@project_data_products_bp.route('', methods=['GET'])
+@assesment_data_products_bp.route('', methods=['GET'])
 @login_required
-def get(project_id: str):
-    """Return a page for managing data products associated with a project."""
-    project = db.get_or_404(Survey, project_id)
-    project_data_product = SurveyDataProduct(survey_id=project.id)
+def get(assesment_id: str):
+    """Return a page for managing data products associated with a assesment."""
+    assesment = db.get_or_404(Survey, assesment_id)
+    assesment_data_product = SurveyDataProduct(survey_id=assesment.id)
 
-    form = Form(obj=project_data_product)
+    form = Form(obj=assesment_data_product)
     return render_template(
-        'project/data_products.html',
+        'assesment/data_products.html',
         form=form,
-        project=project,
-        data_products=project.data_products,
-        sankey_series=sankey_subset(project, SurveyDataProduct),
+        assesment=assesment,
+        data_products=assesment.data_products,
+        sankey_series=sankey_subset(assesment, SurveyDataProduct),
     )
 
 
-@project_data_products_bp.route('', methods=['POST'])
+@assesment_data_products_bp.route('', methods=['POST'])
 @login_required
-def post(project_id: str):
-    """Add a new data product to the project's collection."""
+def post(assesment_id: str):
+    """Add a new data product to the assesment's collection."""
     limit_response_editors()
-    project_data_product = SurveyDataProduct(survey_id=project_id)
-    form = Form(request.form, obj=project_data_product)
+    assesment_data_product = SurveyDataProduct(survey_id=assesment_id)
+    form = Form(request.form, obj=assesment_data_product)
 
     if form.validate():
-        form.populate_obj(project_data_product)
-        db.session.add(project_data_product)
+        form.populate_obj(assesment_data_product)
+        db.session.add(assesment_data_product)
         db.session.commit()
 
     return Response(
         status=201,
         headers={
             'HX-Redirect': url_for(
-                'project.view_project_overview',
-                project_id=project_id,
+                'assesment.view_assesment_overview',
+                assesment_id=assesment_id,
             ),
         },
     )
 
 
-@project_data_products_bp.route('/form', methods=['GET'])
+@assesment_data_products_bp.route('/form', methods=['GET'])
 @login_required
-def form(project_id: str):
-    """Return a form to input a data product to add to the project's collection."""
-    project_data_product = SurveyDataProduct(survey_id=project_id)
-    form = Form(obj=project_data_product)
+def form(assesment_id: str):
+    """Return a form to input a data product to add to the assesment's collection."""
+    assesment_data_product = SurveyDataProduct(survey_id=assesment_id)
+    form = Form(obj=assesment_data_product)
 
     return render_template(
-        'project/_data_product.html',
+        'assesment/_data_product.html',
         form=form,
-        project_id=project_id,
+        assesment_id=assesment_id,
     )
