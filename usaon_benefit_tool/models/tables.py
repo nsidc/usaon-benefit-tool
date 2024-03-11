26653b15c2ac374e2b6c4b5487f9195c7435fdb8
Robyn Marowitz <robyn.marowitz@colorado.edu>
2024-03-11 11:44:45 -0600

    Start of new data model work

--- usaon_benefit_tool/models/tables.py
+++ usaon_benefit_tool/models/tables.py
@@ -36,7 +36,7 @@ class IORelationship(TypedDict):
     input: NotRequired[BaseModel]
     output: NotRequired[BaseModel]
 
-
+# TODO: IOLinkMixin?
 class IORelationshipMixin:
     """Provide a dictionary of input and/or output models related to this entity.
 
@@ -66,6 +66,7 @@ class IORelationshipMixin:
         return io
 
 
+# TODO: object -> node
 class AssessmentObjectFieldMixin:
     """Provide shared fields between all relationship objects to reduce repetition."""
 
@@ -76,12 +77,9 @@ class AssessmentObjectFieldMixin:
     funding_country = Column(String(256), nullable=False)
     # do we want another website field?
     website = Column(String(256), nullable=True)
-    description = Column(String(512), nullable=True)
     contact_information = Column(String(256), nullable=False)
     persistent_identifier = Column(String(256), nullable=True)
-    tags = Column(String, nullable=False)
     real = Column(Boolean, nullable=False)
-    version = Column(String(64), nullable=True)
 
 
 class User(BaseModel, UserMixin):
@@ -185,32 +183,13 @@ class Assessment(BaseModel):
         back_populates='assessments',
     )
     status = relationship('Status')
-
-    observing_systems = relationship(
-        'AssessmentObservingSystem',
-        back_populates='assessment',
-    )
-    data_products = relationship(
-        'AssessmentDataProduct',
-        back_populates='assessment',
-    )
-    applications = relationship(
-        'AssessmentApplication',
-        back_populates='assessment',
-    )
-    societal_benefit_areas = relationship(
-        'AssessmentSocietalBenefitArea',
+    nodes = relationship(
+        'NodeAssessment',
         back_populates='assessment',
     )
 
-
-class AssessmentObservingSystem(
-    BaseModel,
-    IORelationshipMixin,
-    AssessmentObjectFieldMixin,
-):
-    __tablename__ = 'assessment_observing_system'
-    __table_args__ = (UniqueConstraint('short_name', 'assessment_id'),)
+class Node(BaseModel, IORelationshipMixin, AssessmentObjectFieldMixin):
+    __tablename__ = 'node'
     id = Column(
         Integer,
         primary_key=True,
@@ -221,62 +200,42 @@ class AssessmentObservingSystem(
         ForeignKey('assessment.id'),
         nullable=False,
     )
-
-    type = Column(
-        Enum(ObservingSystemType),
+    type_id = Column(
+        String,
+        ForeignKey('node_type.id'),
         nullable=False,
     )
-
-    __mapper_args__: Final[dict] = {
-        'polymorphic_identity': ObservingSystemType.other,
-        'polymorphic_on': type,
-    }
-
+    description = Column(
+        String(512),
+        nullable=True,
+    )
+    tags = Column(String, nullable=False)
+    version = Column(String(64), nullable=True)
+    created_by_id = Column(
+        Integer,
+        ForeignKey('user.id'),
+        default=(lambda: current_user.id),
+        nullable=False,
+    )
+    created_timestamp = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.now,
+    )
+    updated_timestamp = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.now,
+    )
     assessment = relationship(
         'Assessment',
-        back_populates='observing_systems',
-    )
-    output_relationships = relationship(
-        'AssessmentObservingSystemDataProduct',
-        back_populates='observing_system',
-        cascade="all, delete",
+        back_populates='nodes',
     )
 
 
-class AssessmentObservingSystemObservational(BaseModel):
-    __tablename__ = 'assessment_observing_system_observational'
-    __mapper_args__: Final[dict] = {
-        'polymorphic_identity': ObservingSystemType.observational,
-    }
-
-    assessment_observing_system_id = Column(
-        Integer,
-        ForeignKey('assessment_observing_system.id'),
-        primary_key=True,
-    )
-
-    platform = Column(String(256), nullable=False)
-    sensor = Column(String(256), nullable=False)
-
-
-class AssessmentObservingSystemResearch(BaseModel):
-    __tablename__ = 'assessment_observing_system_research'
-    __mapper_args__: Final[dict] = {
-        'polymorphic_identity': ObservingSystemType.research,
-    }
-
-    assessment_observing_system_id = Column(
-        Integer,
-        ForeignKey('assessment_observing_system.id'),
-        primary_key=True,
-    )
-
-    intermediate_product = Column(String(256), nullable=False)
-
-
-class AssessmentDataProduct(BaseModel, IORelationshipMixin, AssessmentObjectFieldMixin):
-    __tablename__ = 'assessment_data_product'
-    __table_args__ = (UniqueConstraint('short_name', 'assessment_id'),)
+class AssessmentNode(BaseModel, IORelationshipMixin, AssessmentObjectFieldMixin):
+    __tablename__ = ''
+    __table_args__ = (UniqueConstraint('short_name', 'assessment_id', 'node_id'),)
     id = Column(
         Integer,
         primary_key=True,
@@ -286,99 +245,6 @@ class AssessmentDataProduct(BaseModel, IORelationshipMixin, AssessmentObjectFiel
         Integer,
         ForeignKey('assessment.id'),
         nullable=False,
-    )
-
-    assessment = relationship(
-        'Assessment',
-        back_populates='data_products',
-    )
-    input_relationships = relationship(
-        'AssessmentObservingSystemDataProduct',
-        back_populates='data_product',
-        cascade="all, delete",
-    )
-    output_relationships = relationship(
-        'AssessmentDataProductApplication',
-        back_populates='data_product',
-        cascade="all, delete",
-    )
-
-
-class AssessmentApplication(BaseModel, IORelationshipMixin, AssessmentObjectFieldMixin):
-    __tablename__ = 'assessment_application'
-    __table_args__ = (UniqueConstraint('short_name', 'assessment_id'),)
-    id = Column(
-        Integer,
-        primary_key=True,
-        autoincrement=True,
-    )
-    assessment_id = Column(
-        Integer,
-        ForeignKey('assessment.id'),
-        nullable=False,
-    )
-
-    # Non-application objects are colored in the diagram based on the weighted average
-    # of their output links. Application objects aren't colored by that criteria because
-    # it wouldn't be fair to judge poor performance in the disaster preparedness SBA for
-    # an app that's not designed for that purpose. Instead, the color of the application
-    # is based on the below performance rating field, and that rating is based on the
-    # stated mission in the performance criteria field.
-    performance_criteria = Column(String(256))
-    performance_rating = Column(
-        Integer,
-        CheckConstraint(
-            'performance_rating>0 and performance_rating<101',
-            name='0-100',
-        ),
-        nullable=False,
-    )
-
-    assessment = relationship(
-        'Assessment',
-        back_populates='applications',
-    )
-    input_relationships = relationship(
-        'AssessmentDataProductApplication',
-        back_populates='application',
-        cascade="all, delete",
-    )
-    output_relationships = relationship(
-        'AssessmentApplicationSocietalBenefitArea',
-        back_populates='application',
-        cascade="all, delete",
-    )
-
-
-class AssessmentSocietalBenefitArea(BaseModel, IORelationshipMixin):
-    __tablename__ = 'assessment_societal_benefit_area'
-    __table_args__ = (UniqueConstraint('societal_benefit_area_id', 'assessment_id'),)
-    id = Column(
-        Integer,
-        primary_key=True,
-        autoincrement=True,
-    )
-    assessment_id = Column(
-        Integer,
-        ForeignKey('assessment.id'),
-        nullable=False,
-    )
-
-    societal_benefit_area_id = Column(
-        String(256),
-        ForeignKey('societal_benefit_area.id'),
-        nullable=False,
-    )
-
-    assessment = relationship(
-        'Assessment',
-        back_populates='societal_benefit_areas',
-    )
-    societal_benefit_area = relationship('SocietalBenefitArea')
-    input_relationships = relationship(
-        'AssessmentApplicationSocietalBenefitArea',
-        back_populates='societal_benefit_area',
-        cascade="all, delete",
     )
 
 
@@ -389,9 +255,10 @@ class Status(BaseModel):
     id = Column(
         String,
         primary_key=True,
+    type = Column(
+        Enum(),
         nullable=False,
-    )
-
+    )    
 
 class Role(BaseModel):
     __tablename__ = 'role'
@@ -401,194 +268,13 @@ class Role(BaseModel):
         nullable=False,
     )
 
-
-class AssessmentObservingSystemDataProduct(BaseModel):
-    __tablename__ = 'assessment_observing_system_data_product'
-    __table_args__ = (
-        UniqueConstraint(
-            'assessment_observing_system_id',
-            'assessment_data_product_id',
-        ),
-        Index(
-            f'idx_{__tablename__}',
-            'assessment_observing_system_id',
-            'assessment_data_product_id',
-            unique=True,
-        ),
-    )
+class Node_Type(BaseModel):
+    __tablename__ = 'role'
     id = Column(
-        Integer,
-        autoincrement=True,
+        String,
         primary_key=True,
-    )
-    assessment_observing_system_id = Column(
-        Integer,
-        ForeignKey('assessment_observing_system.id'),
-        index=True,
-    )
-    assessment_data_product_id = Column(
-        Integer,
-        ForeignKey('assessment_data_product.id'),
-        index=True,
-    )
-
-    criticality_rating = Column(
-        SmallInteger,
-        CheckConstraint(
-            'criticality_rating>=0 and criticality_rating<=100',
-            name='c0-100',
-        ),
         nullable=False,
     )
-    performance_rating = Column(
-        SmallInteger,
-        CheckConstraint(
-            'performance_rating>=0 and performance_rating<=100',
-            name='0-100',
-        ),
-        nullable=False,
-    )
-    rationale = Column(String(512), nullable=True)
-    gaps = Column(String(512), nullable=True)
-
-    observing_system = relationship(
-        'AssessmentObservingSystem',
-        back_populates='output_relationships',
-    )
-    data_product = relationship(
-        'AssessmentDataProduct',
-        back_populates='input_relationships',
-    )
-
-    @property
-    def source(self):
-        return self.observing_system
-
-    @property
-    def target(self):
-        return self.data_product
-
-
-class AssessmentDataProductApplication(BaseModel):
-    __tablename__ = 'assessment_data_product_application'
-    __table_args__ = (
-        UniqueConstraint('assessment_data_product_id', 'assessment_application_id'),
-        Index(
-            f'idx_{__tablename__}',
-            'assessment_data_product_id',
-            'assessment_application_id',
-            unique=True,
-        ),
-    )
-    id = Column(
-        Integer,
-        autoincrement=True,
-        primary_key=True,
-    )
-
-    assessment_data_product_id = Column(
-        Integer,
-        ForeignKey('assessment_data_product.id'),
-        index=True,
-    )
-    assessment_application_id = Column(
-        Integer,
-        ForeignKey('assessment_application.id'),
-        index=True,
-    )
-
-    criticality_rating = Column(
-        SmallInteger,
-        CheckConstraint(
-            'criticality_rating>=0 and criticality_rating<=100',
-            name='c0-100',
-        ),
-        nullable=False,
-    )
-    performance_rating = Column(
-        SmallInteger,
-        CheckConstraint(
-            'performance_rating>=0 and performance_rating<=100',
-            name='0-100',
-        ),
-        nullable=False,
-    )
-    rationale = Column(String(512), nullable=True)
-    needed_improvements = Column(String(512), nullable=True)
-
-    data_product = relationship(
-        'AssessmentDataProduct',
-        back_populates='output_relationships',
-    )
-    application = relationship(
-        'AssessmentApplication',
-        back_populates='input_relationships',
-    )
-
-    @property
-    def source(self):
-        return self.data_product
-
-    @property
-    def target(self):
-        return self.application
-
-
-class AssessmentApplicationSocietalBenefitArea(BaseModel):
-    __tablename__ = 'assessment_application_societal_benefit_area'
-    __table_args__ = (
-        UniqueConstraint(
-            'assessment_application_id',
-            'assessment_societal_benefit_area_id',
-        ),
-        Index(
-            'idx_{__tablename__}',
-            'assessment_application_id',
-            'assessment_societal_benefit_area_id',
-            unique=True,
-        ),
-    )
-    id = Column(
-        Integer,
-        autoincrement=True,
-        primary_key=True,
-    )
-    assessment_application_id = Column(
-        Integer,
-        ForeignKey('assessment_application.id'),
-        index=True,
-    )
-    assessment_societal_benefit_area_id = Column(
-        Integer,
-        ForeignKey('assessment_societal_benefit_area.id'),
-        index=True,
-    )
-
-    performance_rating = Column(
-        SmallInteger,
-        CheckConstraint(
-            'performance_rating>=0 and performance_rating<=100',
-            name='0-100',
-        ),
-        nullable=False,
-    )
-
-    application = relationship(
-        'AssessmentApplication',
-        back_populates='output_relationships',
-    )
-    societal_benefit_area = relationship(
-        'AssessmentSocietalBenefitArea',
-        back_populates='input_relationships',
-    )
-
-    @property
-    def source(self):
-        return self.application
-
-    @property
-    def target(self):
-        return self.societal_benefit_area
 
 
 # Reference tables
@@ -645,9 +331,11 @@ class SocietalBenefitKeyObjective(BaseModel):
     )
 
 
+# NOTE: not sure if we need this
 AssessmentNode = (
     AssessmentObservingSystem
     | AssessmentDataProduct
     | AssessmentApplication
     | AssessmentSocietalBenefitArea
 )
+    )
