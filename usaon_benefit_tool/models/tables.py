e87778c3dc1df7379f20ceb557a7ef3b41324f06
Robyn Marowitz <robyn.marowitz@colorado.edu>
2024-10-02 10:26:36 -0600

    Update names for last 2 fields

--- usaon_benefit_tool/models/tables.py
+++ usaon_benefit_tool/models/tables.py
@@ -414,8 +414,8 @@ class Link(BaseModel):
     )
     performance_rating_rationale = Column(String(8192), nullable=True)
     critically_rating_rationale = Column(String(8192), nullable=True)
-    gaps = Column(String(8192), nullable=True)
-    variable_or_attribute = Column(String(512), nullable=True)
+    gaps_description = Column(String(8192), nullable=True)
+    attribute_description = Column(String(512), nullable=True)
 
     source_assessment_node = relationship(
         AssessmentNode,
